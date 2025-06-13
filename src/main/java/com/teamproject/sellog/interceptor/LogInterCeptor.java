cded73491f5873f8aeb7fe6ee10cbdbc6ee593d1
jsh9298 <jsh9298@gmail.com>
2025-06-13 18:01:54 +0900

    필터 인터셉터 로깅 처리

--- src/main/java/com/teamproject/sellog/interceptor/LogInterCeptor.java
+++ src/main/java/com/teamproject/sellog/interceptor/LogInterCeptor.java
@@ -1,12 +1,14 @@
 package com.teamproject.sellog.interceptor;
 
 import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@RequestMapping
 public class LogInterCeptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
